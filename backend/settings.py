40683ed33cd31803a01c6d32fd10df64d682deb0
pradanadan <dannyardi.danny@gmail.com>
2022-03-27 22:25:07 +0700

    config cors_trusted setting

--- backend/settings.py
+++ backend/settings.py
@@ -68,7 +68,7 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS: True
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', 'https://backend-sport-space.herokuapp.com']
 
 ROOT_URLCONF = 'backend.urls'
 
